87271201cadb925d9e0de9d3a6ef7a27a0f63ee6
Alex Baptista <01abaptista@gmail.com>
2023-11-03 16:16:16 -0400

    Refactored code

--- RTS_Prototype/Assets/Scripts/Type/Selectable.cs
+++ RTS_Prototype/Assets/Scripts/Type/Selectable.cs
@@ -6,11 +6,11 @@ public class Selectable : MonoBehaviour
 {
     public bool isSelected = false;
     [HideInInspector] public float health;
-    [HideInInspector] public enum unitTypes {Robot, Dinosaur, Dead};
+    [HideInInspector] public enum unitTypes { Robot, Dinosaur, Dead };
     [HideInInspector] public unitTypes unitType;
 
 
-    public void DrawCircle(GameObject selected, float radius, 
+    public void DrawCircle(GameObject selected, float radius,
         float lineWidth, Color color)
     {
         var segments = 360;
